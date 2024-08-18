56be9db8fd26f026bbfcfbc13be814879bfbaaac
BorisEdison <borisedison@gmail.com>
2024-08-18 21:53:24 +0530

    minor changes

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -1,5 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using Hawk.Models;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
 
 namespace Hawk.Data 
 {
@@ -11,5 +12,15 @@ namespace Hawk.Data
 
         }
         public DbSet<Category> Categories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasData(
+                new Category {Id=1, Name="Action", DisplayOrder = 1},
+                new Category {Id=2, Name="SciFi", DisplayOrder = 2},
+                new Category {Id=3, Name="Hisrory", DisplayOrder = 3}
+
+            );
+        }
     }
 }
